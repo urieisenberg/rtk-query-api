7f8836883fc0b39770f29f2095a97a0c6a9d4292
urieisenberg <urieisenberg@gmail.com>
2023-01-19 19:22:07 +0200

    render the list

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,6 +1,26 @@
 import { useFetchAlbumsQuery } from '../store';
+import { Skeleton } from './Skeleton';
+import { ExpandablePanel } from './ExpandablePanel';
+import { Button } from './Button';
 
 export const AlbumsList = ({ user }) => {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
-  return <div>AlbumsList</div>;
+
+  let content;
+  if (isLoading) content = <Skeleton times={3} />;
+  else if (error) content = <div>Something went wrong</div>;
+  else if (data.length === 0) content = <div>No albums found</div>;
+  else
+    content = data.map((album) => (
+      <ExpandablePanel key={album.id} header={album.title}>
+        list of photos
+      </ExpandablePanel>
+    ));
+
+  return (
+    <>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </>
+  );
 };
