40cbd682dd9d2cad6451e776223404988dc80f76
urieisenberg <urieisenberg@gmail.com>
2023-01-19 17:08:04 +0200

    use ExpandablePanel

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -1,20 +1,24 @@
+import { Fragment } from 'react';
 import { deleteUser } from '../store';
 import { useThunk } from '../hooks/useThunk';
 import { GoTrashcan } from 'react-icons/go';
 import { Button } from './Button';
+import { ExpandablePanel } from './ExpandablePanel';
 
 export const UsersListItem = ({ user }) => {
   const [doDeleteUser, isLoading, error] = useThunk(deleteUser);
 
   const onDeleteUser = () => doDeleteUser(user);
 
-  return (
-    <>
+  const header = (
+    <Fragment>
       <Button loading={isLoading} onClick={onDeleteUser} className="mr-2">
         <GoTrashcan />
       </Button>
       {error && <div> Something went wrong </div>}
       {user.name}
-    </>
+    </Fragment>
   );
+
+  return <ExpandablePanel header={header}>{user.email}</ExpandablePanel>;
 };
