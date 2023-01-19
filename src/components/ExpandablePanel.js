79bfad4b6ae3e4ae988ad50a8c68e56d4446eb3b
urieisenberg <urieisenberg@gmail.com>
2023-01-19 17:03:33 +0200

    header n children props

--- src/components/ExpandablePanel.js
+++ src/components/ExpandablePanel.js
@@ -2,8 +2,11 @@ export const ExpandablePanel = ({ header, children }) => {
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between"></div>
+        <div className="flex flex-row items-center justify-between">
+          {header}
+        </div>
       </div>
+      <div className="p-2 border-t">{children}</div>
     </div>
   );
 };
