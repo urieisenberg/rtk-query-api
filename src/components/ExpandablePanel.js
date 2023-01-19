1dc01947b722ee70efe07abbf9ede6363df5448b
urieisenberg <urieisenberg@gmail.com>
2023-01-19 17:03:07 +0200

    init ExpandablePanel comp

---
+++ src/components/ExpandablePanel.js
@@ -0,0 +1,9 @@
+export const ExpandablePanel = ({ header, children }) => {
+  return (
+    <div className="mb-2 border rounded">
+      <div className="flex p-2 justify-between items-center cursor-pointer">
+        <div className="flex flex-row items-center justify-between"></div>
+      </div>
+    </div>
+  );
+};
