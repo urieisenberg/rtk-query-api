82dd2a556d822a1d5a93c08ca0e623450c122a2e
urieisenberg <urieisenberg@gmail.com>
2023-01-19 17:10:31 +0200

    make the panel expandable

--- src/components/ExpandablePanel.js
+++ src/components/ExpandablePanel.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
+
 export const ExpandablePanel = ({ header, children }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const onExpand = () => setIsExpanded(!isExpanded);
+
   return (
     <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div className="flex p-2 justify-between items-center">
         <div className="flex flex-row items-center justify-between">
           {header}
         </div>
+        <div onClick={onExpand} className="cursor-pointer">
+          {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+        </div>
       </div>
-      <div className="p-2 border-t">{children}</div>
+      {isExpanded && <div className="p-2 border-t">{children}</div>}
     </div>
   );
 };
