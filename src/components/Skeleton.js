573ae0f0f2bd4aaad6ea79f562718e11eae6357a
urieisenberg <urieisenberg@gmail.com>
2023-01-19 16:05:40 +0200

    for loop

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,3 +1,11 @@
 import classNames from 'classnames';
 
-export const Skeleton = ({ times }) => {};
+export const Skeleton = ({ times }) => {
+  const boxes = [];
+
+  for (let i = 0; i < times; i++) {
+    boxes.push(<div key={i} />);
+  }
+
+  return boxes;
+};
