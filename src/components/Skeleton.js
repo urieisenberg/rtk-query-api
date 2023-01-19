96e883ad45ba38a2a89d479e7a94d184f2725fb8
urieisenberg <urieisenberg@gmail.com>
2023-01-19 16:09:58 +0200

    outerClassNames for outer div

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,13 +1,21 @@
 import classNames from 'classnames';
 
 export const Skeleton = ({ times }) => {
+  const outerClassNames = classNames(
+    'relative',
+    'overflow-hidden',
+    'bg-gray-200',
+    'rounded',
+    'mb-2.5'
+  );
+  const innerClassNames = classNames();
+
   const boxes = Array(times)
     .fill(0)
     .map((_, index) => (
-      <div
-        key={index}
-
-      />
+      <div key={index} className={outerClassNames}>
+        <div className={innerClassNames}></div>
+      </div>
     ));
 
   return boxes;
