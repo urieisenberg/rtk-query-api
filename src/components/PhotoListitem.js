c3e7de1a5273133a3ceb2e44212157589a0afa41
urieisenberg <urieisenberg@gmail.com>
2023-01-20 00:39:30 +0200

    adding mouse over deletes

--- src/components/PhotoListitem.js
+++ src/components/PhotoListitem.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import { GoTrashcan } from 'react-icons/go';
+import { useDeletePhotoMutation } from '../store';
 
 export const PhotoListitem = ({ photo }) => {
+  const [deletePhoto] = useDeletePhotoMutation();
+
+  const onDeletePhoto = () => deletePhoto(photo);
+
   return (
-    <div className="realtive m-2">
+    <div onClick={onDeletePhoto} className="realtive m-2 cursor-pointer">
       <img className="h-2 w-20" src={photo.url} alt="random-faker-pic" />
+      <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity 0 hover:opacity-80">
+        <GoTrashcan className="text-2xl" />
+      </div>
     </div>
   );
 };
