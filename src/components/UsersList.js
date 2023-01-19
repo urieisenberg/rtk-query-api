ed085cc4f3f25c6a50650c3cd4ddb8c936868076
urieisenberg <urieisenberg@gmail.com>
2023-01-19 16:03:14 +0200

    useSelector

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,9 +1,10 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
 
 export const UsersList = () => {
   const dispatch = useDispatch();
+  const { data, isLoading, error } = useSelector((state) => state.users);
 
   useEffect(() => {
     dispatch(fetchUsers());
