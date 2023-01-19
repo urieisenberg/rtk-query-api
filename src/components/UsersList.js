c35ef207ce3a7037dbbea31ff43131d852e42bf3
urieisenberg <urieisenberg@gmail.com>
2023-01-19 16:50:07 +0200

    better display with content var

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -17,28 +17,29 @@ export const UsersList = () => {
 
   const onAddUser = () => doAddUser();
 
-  if (isLoadingUsers) return <Skeleton times={7} className="h-10 w-full" />;
+  let content;
+  if (isLoadingUsers) content = <Skeleton times={7} className="h-10 w-full" />;
 
-  if (loadingUsersError) return <div>Something went wrong</div>;
-
-  const renderedUsers = data.map((user) => (
-    <div key={user.id} className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        {user.name}
+  if (loadingUsersError) content = <div>Something went wrong</div>;
+  else
+    content = data.map((user) => (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
       </div>
-    </div>
-  ));
+    ));
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between item-center m-3">
         <h1 className="m-2 text-xl">Users</h1>
         <Button loading={isCreatingUser} onClick={onAddUser}>
           Add User
         </Button>
         {creatingUserError && <div>{creatingUserError}</div>}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 };
