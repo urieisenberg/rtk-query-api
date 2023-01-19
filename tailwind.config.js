ae7a86a8c0bbeb7c805a59c3d375658688757004
urieisenberg <urieisenberg@gmail.com>
2023-01-19 16:08:40 +0200

    animation w Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-      extend: {},
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      keyframes: {
+        shimmer: {
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+      animation: {
+        shimmer: 'shimmer 1.5s infinite',
+      },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
