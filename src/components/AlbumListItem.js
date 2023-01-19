d4494e3f1364b8669f82e39dde58d8ca7770cb93
urieisenberg <urieisenberg@gmail.com>
2023-01-19 19:43:01 +0200

    header

--- src/components/AlbumListItem.js
+++ src/components/AlbumListItem.js
@@ -1,9 +1,19 @@
+import { GoTrashcan } from 'react-icons/go';
 import { Button } from './Button';
 import { ExpandablePanel } from './ExpandablePanel';
 
 export const AlbumListItem = ({ album }) => {
+  const header = (
+    <div>
+      <Button>
+        <GoTrashcan />
+      </Button>
+      {album.title}
+    </div>
+  );
+
   return (
-    <ExpandablePanel key={album.id} header={album.title}>
+    <ExpandablePanel key={album.id} header={header}>
       list of photos
     </ExpandablePanel>
   );
