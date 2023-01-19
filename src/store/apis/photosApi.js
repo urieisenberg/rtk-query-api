77b9cadba92f775ca93a763a2278b8c3e800051b
urieisenberg <urieisenberg@gmail.com>
2023-01-20 00:25:11 +0200

    adding endpoints for fetch, delete n create

--- src/store/apis/photosApi.js
+++ src/store/apis/photosApi.js
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { faker } from '@faker-js/faker';
 
 export const photosApi = createApi({
   reducerPath: 'photos',
@@ -7,9 +8,36 @@ export const photosApi = createApi({
   }),
   endpoints(builder) {
     return {
-      fetchPhotos: builder.query({}),
-      addPhoto: builder.mutation({}),
-      deletePhoto: builder.mutation({}),
+      fetchPhotos: builder.query({
+        query: (album) => {
+          return {
+            url: '/photos',
+            params: { albumId: album.id },
+            method: 'GET',
+          };
+        },
+      }),
+      addPhoto: builder.mutation({
+        query: (album) => {
+          return {
+            url: '/photos',
+            method: 'POST',
+            body: {
+              albumId: album.id,
+              title: faker.image.abstract(150, 150, true), // 150x150 image with a random title (true)
+            },
+          };
+        },
+      }),
+      deletePhoto: builder.mutation({
+        query: (photo) => {
+          return {
+            url: `/photos/${photo.id}`,
+            method: 'DELETE',
+          };
+        },
+      }),
     };
   },
 });
+
