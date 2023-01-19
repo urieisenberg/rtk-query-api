befdd24805d7c461a25d12e3ee0096c2267bed7f
urieisenberg <urieisenberg@gmail.com>
2023-01-19 17:03:03 +0200

    divs moved to expandablepanel comp

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -9,16 +9,12 @@ export const UsersListItem = ({ user }) => {
   const onDeleteUser = () => doDeleteUser(user);
 
   return (
-    <div key={user.id} className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-          <Button loading={isLoading} onClick={onDeleteUser} className="mr-2">
-            <GoTrashcan />
-          </Button>
-          {error && <div> Something went wrong </div>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+    <>
+      <Button loading={isLoading} onClick={onDeleteUser} className="mr-2">
+        <GoTrashcan />
+      </Button>
+      {error && <div> Something went wrong </div>}
+      {user.name}
+    </>
   );
 };
