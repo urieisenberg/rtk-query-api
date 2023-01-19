b93667022d57bec02a2476e8bbfccc95f4003810
urieisenberg <urieisenberg@gmail.com>
2023-01-20 00:31:15 +0200

    add photo

--- src/components/Photolist.js
+++ src/components/Photolist.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import { useFetchPhotosQuery, useAddPhotoMutation } from '../store';
+import { Button } from './Button';
 
 export const Photolist = ({ album }) => {
-  return <div>Photolist</div>;
+  const { data: photos, isLoading } = useFetchPhotosQuery(album);
+  const [addPhoto, { isLoading: isAdding }] = useAddPhotoMutation();
+
+  const onAddPhoto = () => addPhoto(album);
+
+  return (
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Photos in {album.title}</h3>
+        <Button onClick={onAddPhoto} loading={isAdding}>
+          Add Photo
+        </Button>
+      </div>
+    </div>
+  );
 };
