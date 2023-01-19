b34926a6834841b4dd98d411112a9601567d25e9
urieisenberg <urieisenberg@gmail.com>
2023-01-19 18:05:44 +0200

    export query hook from albumsApi

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { usersReducer } from './slices/usersSlice';
-import { albumsApi } from './apis/albumsApi';
+import { albumsApi, useFetchAlbumsQuery } from './apis/albumsApi';
 
 export const store = configureStore({
   reducer: {
@@ -18,3 +18,4 @@ setupListeners(store.dispatch);
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/deleteUser';
+export { useFetchAlbumsQuery } from './apis/albumsApi';
