1bfdeef010909d8684d2835b02cc4ffdc30b3153
urieisenberg <urieisenberg@gmail.com>
2023-01-19 16:00:10 +0200

    export the thunk from index

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { usersReducer } from './slices/usersSlice';
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     users: usersReducer,
   },
 });
 
-export { store };
+export * from './thunks/fetchUsers';
