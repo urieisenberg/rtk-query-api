131d67718cd2087058a5a64954b736a347d698db
urieisenberg <urieisenberg@gmail.com>
2023-01-19 17:07:22 +0200

    // added to fix err

--- src/store/thunks/deleteUser.js
+++ src/store/thunks/deleteUser.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const deleteUser = createAsyncThunk('users/deleteUser', async (user) => {
-  await axios.delete(`http:localhost:3005/users/${user.id}`);
+  await axios.delete(`http://localhost:3005/users/${user.id}`);
   return user;
 });
 
