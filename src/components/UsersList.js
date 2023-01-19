745af935ce0f9fce8629dc9eb5a0b70b3197a54b
urieisenberg <urieisenberg@gmail.com>
2023-01-19 16:01:49 +0200

    dispatch the thunk func when user does something

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -3,5 +3,11 @@ import { useDispatch } from 'react-redux';
 import { fetchUsers } from '../store';
 
 export const UsersList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return <></>;
 };
