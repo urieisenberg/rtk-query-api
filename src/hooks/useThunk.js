67f392133638f757a3ea877e04568da7450b840f
urieisenberg <urieisenberg@gmail.com>
2023-01-19 16:36:47 +0200

    useCallback for reusable thunk hook

--- src/hooks/useThunk.js
+++ src/hooks/useThunk.js
@@ -1,17 +1,20 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 
 export function useThunk(thunk) {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const dispatch = useDispatch();
 
-  const runThunk = () => {
+  const runThunk = useCallback(() => {
     setIsLoading(true);
-    return thunk()
-      .unwrap() // Unwrap the promise to get the actual data from the fulfilled action payload
-      .catch((error) => setError(error.message)) // Catch the error from the rejected action payload
-      .finally(() => setIsLoading(false)); // This will run regardless of the promise being fulfilled or rejected
-  };
+    dispatch(
+      thunk()
+        .unwrap() // Unwrap the promise to get the actual data from the fulfilled action payload
+        .catch((error) => setError(error.message)) // Catch the error from the rejected action payload
+        .finally(() => setIsLoading(false))
+    ); // This will run regardless of the promise being fulfilled or rejected
+  }, [thunk, dispatch]);
 
   return { isLoading, error, runThunk };
 }
-
