e0740d7fbdb8132108f08cc78748662d8f687295
urieisenberg <urieisenberg@gmail.com>
2023-01-19 17:52:47 +0200

    add fetchAlbums endpoint

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -5,4 +5,17 @@ export const albumsApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:3005',
   }),
+  endpoints(builder) {
+    return {
+      fetchAlbums: builder.query({
+        query: (user) => {
+          return {
+            url: '/albums',
+            params: { userId: user.id },
+            method: 'GET',
+          };
+        },
+      }),
+    };
+  },
 });
