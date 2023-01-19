1aa8d16e912497e3fd76b372616a7999aaa1d6e1
urieisenberg <urieisenberg@gmail.com>
2023-01-19 17:58:01 +0200

    setupListeners for rtk query

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import { usersReducer } from './slices/usersSlice';
 import { albumsApi } from './apis/albumsApi';
 
@@ -11,6 +12,8 @@ export const store = configureStore({
     return getDefaultMiddleware().concat(albumsApi.middleware);
   },
 });
+
+setupListeners(store.dispatch);
 
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
